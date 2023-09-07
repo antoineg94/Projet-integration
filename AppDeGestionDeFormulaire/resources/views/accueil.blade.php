d3bf9f6f8e1380f80844edd9ef0d94567e8b6375
antoineg94 <117280367+antoineg94@users.noreply.github.com>
2023-09-07 16:20:44 -0400

    correction bouton accueil

--- AppDeGestionDeFormulaire/resources/views/accueil.blade.php
+++ AppDeGestionDeFormulaire/resources/views/accueil.blade.php
@@ -5,9 +5,9 @@
 @section('middleContent')
 <section>
 <div class="d-grid gap-3 col-10 mx-auto p-2">
-<a class="btn text-white" style="background-color: #63BC55;" type="button" href="{{ route('formulairesTravails.index') }}">Formulaire déclaration d'accidents de travail</a>
+<a class="btn text-white" style="background-color: #63BC55;" type="button" href="{{ route('formulairesTravails.index') }}">Déclaration d'accidents de travail</a>
 <a class="btn text-white" style="background-color: #63BC55;"type="button" href="{{ route('formulaireSituationDangereuses.index') }}">Formulaire situation dangereuse</a>
-<a class="btn text-white" type="button" style="background-color: #63BC55;" href="{{ route('formulaireAudits.index') }}">Formulaire d'audit</a>
+<a class="btn text-white" type="button" style="background-color: #63BC55;" href="{{ route('formulaireAudits.index') }}">Formulaire d'audit SST</a>
 <a class="btn text-white" type="button" style="background-color: #63BC55;" href="{{ route('formulaireMecaniques.index') }}">Formulaire d'analyse mécanique</a>
 
 
