7e6d0e8b01fa32e61b6906ca15d29ee0a846293b
antoineg94 <117280367+antoineg94@users.noreply.github.com>
2023-09-11 10:58:58 -0400

    debut fonction blessure gauche droite

---
+++ AppDeGestionDeFormulaire/public/js/coteBlessure.js
@@ -0,0 +1,9 @@
+// create a function that adds two radio buttons
+function addRadioButtons(divName) {
+    const selectBlessure = document.getElementById("blessure");
+    selectBlessure.addEventListener("change", function () {
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML = "<input type='radio' name='coteBlessure' value='gauche'> Gauche<br><input type='radio' name='coteBlessure' value='droite'> Droite";
+    document.getElementById(divName).appendChild(newdiv);
+    });
+}
