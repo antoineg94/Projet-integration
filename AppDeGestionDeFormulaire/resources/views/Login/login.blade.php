58a801563fd4a6a94c670075b2699eda6bfe6e7a
antoineg94 <117280367+antoineg94@users.noreply.github.com>
2023-09-14 15:10:29 -0400

    login ok

--- AppDeGestionDeFormulaire/resources/views/Login/login.blade.php
+++ AppDeGestionDeFormulaire/resources/views/Login/login.blade.php
@@ -1,13 +1,18 @@
+@extends('layout/app')
+
+@section('title', 'Login')
+
+@section('middleContent')
 <section>
 <form method="POST" action="{{ route('Usagers.login') }}">
     @CSRF
     <div class=" mb-3 pb-1" > 
-        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px; float:right;">Connexion:</h5>
+        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Connexion:</h5>
     </div>
 
     <div class="form-outline mb-4">
-    <label class="form-label" for="email">Adresse courriel</label>
-        <input type="email" id="email" name="email" class="form-control form-control-lg  @error('email') is-invalid @enderror" />
+    <label class="form-label" for="matricule">Matricule</label>
+        <input type="text" id="matricule" name="matricule" class="form-control form-control-lg  @error('matricule') is-invalid @enderror" />
         @error('email')
                 <span class="text-danger">{{ $message }} </span>
                 @enderror
@@ -21,9 +26,10 @@
         @enderror
     </div>
 
-    <div>
-        <a href="#" class="btn w-100">Envoyer</a>
-    </div>               
+    <div class="col-11 mx-auto">
+    <a class="btn d-grid text-white" style="background-color: #63BC55;" type="button" href="">Envoyer</a>
+    </div>                
 </form>
 
 </section>
+@endsection
