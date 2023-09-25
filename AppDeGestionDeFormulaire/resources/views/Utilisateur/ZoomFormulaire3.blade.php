1a3413f9a161a4a94946c5300390be5b00df856f
PatCegepTR <patrice.anctil.01@edu.cegeptr.qc.ca>
2023-09-25 09:34:48 -0400

    Les $formulaire sont enlevé + formulaire 3 code plus clair

--- AppDeGestionDeFormulaire/resources/views/Utilisateur/ZoomFormulaire3.blade.php
+++ AppDeGestionDeFormulaire/resources/views/Utilisateur/ZoomFormulaire3.blade.php
@@ -6,90 +6,85 @@
 <section>
 <div class="d-grid gap-3 col-11 mx-auto p-2">
 @if (isset($zoomForm3s))
-<div class="card mb-3">
-  <h5 class="card-header">
-    Informations sur: {{ $formulaire->nomFormulaire }}
-  </h5>
-  <div class="card-body">
 
   <table class="table table-striped">
-<thead>
-  <tr>
-    <th>Nom</th>
-    <th>{{ $zoomForm3->nom }}, {{ $zoomForm3->prenom }}</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td>Lieux</td>
-    <td>{{ $zoomForm3->lieu }}</td>
-  </tr>
-  <tr>
-    <td>Date</td>
-    <td>{{ $zoomForm3->date_formulaire }}</td>
-  </tr>
-  <tr>
-    <td>Heure</td>
-    <td>{{ $zoomForm3-> }}</td>
-  </tr>
-  <tr>
-    <td>EPI</td>
-    <td>{{ $zoomForm3->epi }}</td>
-  </tr>
-  <tr>
-    <td>Tenue des lieux</td>
-    <td>{{ $zoomForm3tenue }}</td>
-  </tr>
-  <tr>
-    <td>Comportement sécuritaire</td>
-    <td>{{ $zoomForm3->comportement }}</td>
-  </tr>
-  <tr>
-    <td>Signalisation</td>
-    <td>{{ $zoomForm3->signalisation }}</td>
-  </tr>
-  <tr>
-    <td>Fiche signalétiques</td>
-    <td>{{ $zoomForm3->fiche_signal }}</td>
-  </tr>
-  <tr>
-    <td>Travaux - Excavation</td>
-    <td>{{ $zoomForm3->travaux }}</td>
-  </tr>
-  <tr>
-    <td>Espace clos</td>
-    <td>{{ $zoomForm3->espace_clos }}</td>
-  </tr>
-  <tr>
-    <td>Méthode de travail</td>
-    <td>{{ $zoomForm3->methode_travail }}</td>
-  </tr>
-  <tr>
-    <td>Autre</td>
-    <td>{{ $zoomForm3->autre }}</td>
-  </tr>
-  <tr>
-    <td>COVID</td>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>{{ $zoomForm3s->nom }}, {{ $zoomForm3s->prenom }}</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Lieux</td>
+            <td>{{ $zoomForm3s->lieu }}</td>
+        </tr>
+        <tr>
+            <td>Date</td>
+            <td>{{ $zoomForm3s->date_formulaire }}</td>
+        </tr>
+        <tr>
+            <td>Heure</td>
+            <td>{{ $zoomForm3s-> }}</td>
+        </tr>
+        <tr>
+            <td>EPI</td>
+            <td>{{ $zoomForm3s->epi }}</td>
+        </tr>
+        <tr>
+            <td>Tenue des lieux</td>
+            <td>{{ $zoomForm3stenue }}</td>
+        </tr>
+        <tr>
+            <td>Comportement sécuritaire</td>
+            <td>{{ $zoomForm3s->comportement }}</td>
+        </tr>
+        <tr>
+            <td>Signalisation</td>
+            <td>{{ $zoomForm3s->signalisation }}</td>
+        </tr>
+        <tr>
+            <td>Fiche signalétiques</td>
+            <td>{{ $zoomForm3s->fiche_signal }}</td>
+        </tr>
+        <tr>
+            <td>Travaux - Excavation</td>
+            <td>{{ $zoomForm3s->travaux }}</td>
+        </tr>
+        <tr>
+            <td>Espace clos</td>
+            <td>{{ $zoomForm3s->espace_clos }}</td>
+        </tr>
+        <tr>
+            <td>Méthode de travail</td>
+            <td>{{ $zoomForm3s->methode_travail }}</td>
+        </tr>
+        <tr>
+            <td>Autre</td>
+            <td>{{ $zoomForm3s->autre }}</td>
+        </tr>
+        <tr>
+            <td>COVID</td>
 
-  </tr>
-  <tr>
-    <td>respect de la distanciation</td>
-    <td>{{ $zoomForm3->respect_distance }}</td>
-  </tr>
-  <tr>
-    <td>port des EPI (masque/visière)</td>
-    <td>{{ $zoomForm3->port_epi }}</td>
-  </tr>
-  <tr>
-    <td>Respect des procédures établies</td>
-    <td>{{ $zoomForm3->respect_proced }}</td>
-  </tr>
-  <tr>
-    <td>Description</td>
-    <td>{{ $zoomForm3->description }}</td>
-  </tr>
-</tbody>
-</table>
+        </tr>
+        <tr>
+            <td>respect de la distanciation</td>
+            <td>{{ $zoomForm3s->respect_distance }}</td>
+        </tr>
+        <tr>
+            <td>port des EPI (masque/visière)</td>
+            <td>{{ $zoomForm3s->port_epi }}</td>
+        </tr>
+        <tr>
+            <td>Respect des procédures établies</td>
+            <td>{{ $zoomForm3s->respect_proced }}</td>
+        </tr>
+        <tr>
+            <td>Description</td>
+            <td>{{ $zoomForm3s->description }}</td>
+        </tr>
+      </tbody>
+    </table>
 @else
 <p>Une erreur s'est produite, veuillez réessayer plus tard</p>
 @endif
--- AppDeGestionDeFormulaire/resources/views/Utilisateur/ZoomFormulaire4.blade.php
+++ AppDeGestionDeFormulaire/resources/views/Utilisateur/ZoomFormulaire4.blade.php
@@ -6,39 +6,34 @@
 <section>
 <div class="d-grid gap-3 col-11 mx-auto p-2">
 @if (isset($zoomForm4s))
-<div class="card mb-3">
-  <h5 class="card-header">
-    Informations sur: {{ $formulaire->nomFormulaire }}
-  </h5>
-  <div class="card-body">
 
   <table class="table table-striped">
 <thead>
   <tr>
     <th>Numéro(s) d'unité(s) impliqué(s)</th>
-    <th>{{ $zoomForm4->no_unite }}</th>
+    <th>{{ $zoomForm4s->no_unite }}</th>
   </tr>
 </thead>
 <tbody>
   <tr>
     <td>Département</td>
-    <td>{{ $zoomForm4->departement }}</td>
+    <td>{{ $zoomForm4s->departement }}</td>
   </tr>
   <tr>
     <td>Nom de l'employé impliqué</td>
-    <td>{{ $zoomForm4->nom }}, {{ $zoomForm4->prenom }}</td>
+    <td>{{ $zoomForm4s->nom }}, {{ $zoomForm4s->prenom }}</td>
   </tr>
   <tr>
     <td>Nom du supérieur immédiat</td>
-    <td>{{ $zoomForm4->superviseur }}</td>
+    <td>{{ $zoomForm4s->superviseur }}</td>
   </tr>
   <tr>
     <td>Permis de conduire de l'employé</td>
-    <td>{{ $zoomForm4->permis_conduire }}</td>
+    <td>{{ $zoomForm4s->permis_conduire }}</td>
   </tr>
   <tr>
     <td>Autre véhicules impliqués? (citoyen)</td>
-    <td>{{ $zoomForm4->vehicule_citoyen }}</td>
+    <td>{{ $zoomForm4s->vehicule_citoyen }}</td>
   </tr>
 </tbody>
 </table>
