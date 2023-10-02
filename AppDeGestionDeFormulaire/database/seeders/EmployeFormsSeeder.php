bcb644e0dd0a914ba403976e81910c4335ec24ca
PatCegepTR <patrice.anctil.01@edu.cegeptr.qc.ca>
2023-10-02 08:25:10 -0400

    Seeder +++

--- AppDeGestionDeFormulaire/database/seeders/EmployeFormsSeeder.php
+++ AppDeGestionDeFormulaire/database/seeders/EmployeFormsSeeder.php
@@ -16,62 +16,82 @@ class EmployeFormsSeeder extends Seeder
         DB::table('employeforms')->insert([
             [
                 'employe_id' => 2,
-                'formulaire_id' => 4,
+                'formulaire_id' => 1,
                 'date_formulaire' => '2023-04-11' 
             ],
             [
-                'employe_id' => 19,
-                'formulaire_id' => 1,
-                'date_formulaire' => '2023-06-02'             
-            ],
-            [
-                'employe_id' => 23,
-                'formulaire_id' => 1,
-                'date_formulaire' => '2022-01-12'             
-            ],
-            [
-                'employe_id' => 11,
-                'formulaire_id' => 3,
-                'date_formulaire' => '2021-05-12'             
-            ],
-            [
-                'employe_id' => 5,
-                'formulaire_id' => 1,
-                'date_formulaire' => '2022-05-07'             
-            ],
-            [
-                'employe_id' => 8,
-                'formulaire_id' => 1,
-                'date_formulaire' => '2021-02-09'             
-            ],
-            [
-                'employe_id' => 19,
+                'employe_id' => 2,
                 'formulaire_id' => 2,
-                'date_formulaire' => '2023-05-12'            
+                'date_formulaire' => '2023-06-02'             
             ],
             [
                 'employe_id' => 2,
                 'formulaire_id' => 3,
-                'date_formulaire' => '2023-05-18'             
+                'date_formulaire' => '2022-01-12'             
+            ],
+            [
+                'employe_id' => 2,
+                'formulaire_id' => 4,
+                'date_formulaire' => '2021-05-12'             
             ],
             [
                 'employe_id' => 3,
+                'formulaire_id' => 1,
+                'date_formulaire' => '2022-05-07'             
+            ],
+            [
+                'employe_id' => 3,
+                'formulaire_id' => 2,
+                'date_formulaire' => '2021-02-09'             
+            ],
+            [
+                'employe_id' => 3,
+                'formulaire_id' => 3,
+                'date_formulaire' => '2023-05-12'            
+            ],
+            [
+                'employe_id' => 3,
+                'formulaire_id' => 4,
+                'date_formulaire' => '2023-05-18'             
+            ],
+            [
+                'employe_id' => 6,
                 'formulaire_id' => 1,
                 'date_formulaire' => '2023-05-11'            
             ],
             [
                 'employe_id' => 29,
-                'formulaire_id' => 3,
+                'formulaire_id' => 2,
                 'date_formulaire' => '2023-12-11'             
             ],
             [
-                'employe_id' => 3,
+                'employe_id' => 8,
                 'formulaire_id' => 1,
                 'date_formulaire' => '2023-11-12'             
             ],
             [
-                'employe_id' => 18,
+                'employe_id' => 12,
+                'formulaire_id' => 2,
+                'date_formulaire' => '2023-12-22'            
+            ],
+            [
+                'employe_id' => 16,
                 'formulaire_id' => 3,
+                'date_formulaire' => '2023-11-12'             
+            ],
+            [
+                'employe_id' => 18,
+                'formulaire_id' => 4,
+                'date_formulaire' => '2023-12-22'            
+            ],
+            [
+                'employe_id' => 9,
+                'formulaire_id' => 3,
+                'date_formulaire' => '2023-11-12'             
+            ],
+            [
+                'employe_id' => 19,
+                'formulaire_id' => 4,
                 'date_formulaire' => '2023-12-22'            
             ]
         ]);
