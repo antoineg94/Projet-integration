ab9eb6f33c8c4b890cc9238a5414a7e39420b588
natLafreniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2023-10-05 15:55:46 -0400

    rajout regexp form1

--- AppDeGestionDeFormulaire/app/Http/Requests/Form1Request.php
+++ AppDeGestionDeFormulaire/app/Http/Requests/Form1Request.php
@@ -22,14 +22,14 @@ class Form1Request extends FormRequest
     public function rules(): array
     {
         return [
-            'fonction_avant' => 'required',
+            'fonction_avant' => 'required|regex:/^[a-zA-Z]+$/u',
             'date_incident' => 'required',
             'heure_incident' => 'required',
-            'lieu' => 'required',
-            'secteur' => 'required',
+            'lieu' => 'required|regex:/^[a-zA-Z]+$/u',
+            'secteur' => 'required|regex:/^[a-zA-Z]+$/u',
             'nature_blessure' => 'required',
             'description_blessure' => 'required',
-            'description_tache' => 'required',
+            'description_tache' => 'required|regex:/^[a-zA-Z]+$/u',
             'type_violence' => 'required',
             'type_absence' => 'required'
         ];
@@ -43,13 +43,17 @@ class Form1Request extends FormRequest
     {
         return [
             'fonction_avant.required' => 'Veuillez entrer la fonction avant l\'incident',
+            'fonction_avant.regex' => 'Veuillez entrer des caractères valides',
             'date_incident.required' => 'Veuillez entrer la date de l\'incident',
             'heure_incident.required' => 'Veuillez entrer l\'heure de l\'incident',
             'lieu.required' => 'Veuillez entrer le lieu de l\'incident',
+            'lieu.regex' => 'Veuillez entrer des caractères valides',
             'secteur.required' => 'Veuillez entrer le secteur de l\'incident',
+            'secteur.regex' => 'Veuillez entrer des caractères valides',
             'nature_blessure.required' => 'Veuillez entrer la nature de la blessure',
             'description_blessure.required' => 'Veuillez entrer la description de la blessure',
             'description_tache.required' => 'Veuillez entrer la description de la tâche',
+            'description_tache.regex' => 'Veuillez entrer des caractères valides',
             'type_violence.required' => 'Veuillez entrer le type de violence',
             'type_absence.required' => 'Veuillez entrer le type d\'absence'
         ];
