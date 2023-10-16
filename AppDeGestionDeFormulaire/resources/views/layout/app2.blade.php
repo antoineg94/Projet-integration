323c50d93728203a82ace7dbb01c9599a825ad99
natLafreniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2023-10-16 09:12:24 -0400

    page admin deco

--- AppDeGestionDeFormulaire/resources/views/layout/app2.blade.php
+++ AppDeGestionDeFormulaire/resources/views/layout/app2.blade.php
@@ -56,7 +56,7 @@
             <strong>mdo</strong>
           </a>
           <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="#">Déconnexion</a></li>
+            <li><a class="dropdown-item" href="{{route('Logins.logout')}}">Déconnexion</a></li>
           </ul>
         </div>
       </div>
