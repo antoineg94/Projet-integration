dd71df2e6778e4dbb70f073047d352b58486dd6a
natLafreniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2023-11-09 17:02:40 -0500

    app modif

--- AppDeGestionDeFormulaire/resources/views/layout/app2.blade.php
+++ AppDeGestionDeFormulaire/resources/views/layout/app2.blade.php
@@ -31,8 +31,9 @@
     <sidebar class="h-100" style="position: fixed;">
       <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark h-100" style="width: 280px;">
         <a href="{{ route('Menus.pageAdmin') }}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        
-          <span class="fs-4">SST V3R</span>
+          <span class="fs-4">
+            SST V3R
+          </span>
         </a>
         <hr>
         <ul class="nav nav-pills flex-column mb-auto">
@@ -56,20 +57,21 @@
         <hr>
         <div class="dropdown d-flex justify-content-between">
           <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-          
-            <strong>{{ session::get('prenom' )}} {{ session::get('nom' )}}</strong>
+            <strong>
+              {{ session::get('prenom' )}} {{ session::get('nom' )}}
+            </strong>
           </a>
           <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="{{route('Logins.logout')}}">Déconnexion</a></li>
+            <li>
+              <a class="dropdown-item" href="{{route('Logins.logout')}}">
+                Déconnexion
+              </a>
+            </li>
           </ul>
-          
         </div>
       </div>
     </sidebar>
-
-
     <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark h-100" style="width: 280px;">
-
     </div>
     @yield('middleContent')
 
