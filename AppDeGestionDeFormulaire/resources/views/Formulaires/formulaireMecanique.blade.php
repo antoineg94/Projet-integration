c14914fd58fce6d11b026e1f6d520fdac803e19a
natLafreniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2023-11-16 16:38:09 -0500

    oublie secu heure form4

--- AppDeGestionDeFormulaire/resources/views/Formulaires/formulaireMecanique.blade.php
+++ AppDeGestionDeFormulaire/resources/views/Formulaires/formulaireMecanique.blade.php
@@ -31,12 +31,18 @@
     <div class="row">
         <div class="form-outline mb-4 col-xl-6 col-12">
             <label class="form-label fw-bold" for="prenom">Date</label>
-            <input type="date" id="date" name="date" class="form-control form-control-lg" value="{{ Session::get('date') }}"/>
+            <input type="date" id="date" name="date" class="form-control form-control-lg @error('date') is-invalid @enderror" value="{{ Session::get('date') }}"/>
+            @error('date')
+                <span class="text-danger">{{ $message }} </span>
+            @enderror
         </div>
 
         <div class="form-outline mb-4 col-xl-6 col-12">
             <label class="form-label fw-bold" for="prenom">Heure</label>
-            <input type="time" id="heure" name="heure" class="form-control form-control-lg" value="{{ Session::get('heure') }}"/>
+            <input type="time" id="heure" name="heure" class="form-control form-control-lg @error('heure') is-invalid @enderror" value="{{ Session::get('heure') }}"/>
+            @error('heure')
+                <span class="text-danger">{{ $message }} </span>
+            @enderror
         </div>
     </div>
 
