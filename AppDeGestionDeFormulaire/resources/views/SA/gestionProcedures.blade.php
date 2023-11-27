482582fef18387110f9cd695d2895f7efbd1dd94
natLafreniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2023-11-27 08:29:04 -0500

    suppression procédure

--- AppDeGestionDeFormulaire/resources/views/SA/gestionProcedures.blade.php
+++ AppDeGestionDeFormulaire/resources/views/SA/gestionProcedures.blade.php
@@ -87,25 +87,7 @@
                             </div>
                         </div>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Supression d'une procédure de travail</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Êtes vous sur de vouloir supprimer cette procédure? Cette action est irréversible.
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Non</button>
-                                <a role="button" class="btn btn-primary" href="{{route('consulterProcedures.destroy', [$procedure->id]) }}" >Oui</a>
-                            </div>
-                            </div>
-                            </div>
-                        </div>
-                        <!-- -->
+                        
                     @endforeach
                 @else
                     <p>Aucune procédure disponible pour le moment..</p>
