20be4078d52daacea72e52d74f8cba50303a42f3
PatCegepTR <patrice.anctil.01@edu.cegeptr.qc.ca>
2023-11-27 08:33:38 -0500

    grosse correction nom superieur

--- AppDeGestionDeFormulaire/resources/views/SA/listeTousFormulaires.blade.php
+++ AppDeGestionDeFormulaire/resources/views/SA/listeTousFormulaires.blade.php
@@ -43,32 +43,28 @@
             </div>
           </form>
 
-           @foreach($listes as $liste) 
-            @foreach($superieur_noms as $superieur_nom)
-              @if($superieur_nom->id == $liste->superieur_id)
-                <div class="card mb-3 col-10 offset-2 px-0 @if($liste->consulte != 'Consulté') border border-success border-3 @endif" >
-                  <div class="card-header">{{ $liste->nom_formulaire }}</div>
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="card-text col">Nom: <b>{{ $liste->prenom }} {{ $liste->nom }} </b></div>
-                      <div class="card-text col">Date: <b>{{ $liste->date_formulaire }} </b></div>
-                    </div>
-                    <div class="row">
-                      <div class="card-text col">Consulté: <b>{{ $liste->consulte }}</b></div>
-                      <div class="card-text col">Statut: <b>{{ $liste->statut }}</b></div>
-                    </div>
-                    <div class="row">
-                      <div class="card-text col">Supérieur: <b>{{ $superieur_nom->prenom }} {{ $superieur_nom->nom }}</b></div>
-                    </div>
-                      <div class=" d-flex justify-content-end col">
-                        @if (Session::get('admin') == true || Session::get('superviseur') == true)
-                          <a href=" {{route('SA.adminZoomFormulaire', [ $liste ]) }}" class="btn text-white px-5" style="background-color: #63BC55;" >Consulter</a>
-                        @endif
-                      </div>
-                  </div>
+          @foreach($listes as $liste)
+            <div class="card mb-3 col-10 offset-2 px-0 @if($liste->consulte != 'Consulté') border border-success border-3 @endif" >
+              <div class="card-header">{{ $liste->nom_formulaire }}</div>
+              <div class="card-body">
+                <div class="row">
+                  <div class="card-text col">Nom: <b>{{ $liste->prenom }} {{ $liste->nom }} </b></div>
+                  <div class="card-text col">Date: <b>{{ $liste->date_formulaire }} </b></div>
                 </div>
-              @endif
-            @endforeach  
+                <div class="row">
+                  <div class="card-text col">Consulté: <b>{{ $liste->consulte }}</b></div>
+                  <div class="card-text col">Statut: <b>{{ $liste->statut }}</b></div>
+                </div>
+                <div class="row">
+                  <div class="card-text col">Supérieur: <b>{{ $liste->sup_prenom }} {{ $liste->sup_nom }}</b></div>
+                </div>
+                  <div class=" d-flex justify-content-end col">
+                    @if (Session::get('admin') == true || Session::get('superviseur') == true)
+                      <a href=" {{route('SA.adminZoomFormulaire', [ $liste ]) }}" class="btn text-white px-5" style="background-color: #63BC55;" >Consulter</a>
+                    @endif
+                  </div>
+              </div>
+            </div>
           @endforeach  
            
         @else
